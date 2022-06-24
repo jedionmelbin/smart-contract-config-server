f07d57e605008044b3d4d504a559252bbbcf26d6
jedionmelbin <jedion.melbin.paucar.cardenas@everis.com>
2022-06-24 15:31:40 -0500

    first commit

---
+++ src/main/java/com/xprotech/api/SmartContractConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.xprotech.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContractConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartContractConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xprotech/api/SmartContractConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xprotech.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContractConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
